c3a7cbb4db1c4c6073594225372ebf0ae5ed3788
sieunn <73224848+sieunn@users.noreply.github.com>
2024-07-16 17:47:08 +0900

    login

--- Tutorial-Login-Back/src/main/java/com/kh/mapper/TodoMapper.java
+++ Tutorial-Login-Back/src/main/java/com/kh/mapper/TodoMapper.java
@@ -7,7 +7,9 @@ import com.kh.dto.TodoMember;
 @Mapper
 public interface TodoMapper {
 	int idCheck(String id);
-	
 	//int signup(DTO에 만든 TodoMember가져오기);
 	int signup(TodoMember member);
+	
+	TodoMember login(TodoMember member); //id pw만 써도 되고, TodoMember 모두 써도됨
+	//TodoMember login(String id, String pw); //id pw 만 가져와서 로그인 하기
 }
